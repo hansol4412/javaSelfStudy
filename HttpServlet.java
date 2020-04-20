58336d02f3d5e8ba9767b535d95a4a4bcf2c5c5b
Hansol <hansol4412@naver.com>
2020-04-20 15:26:09 +0900

    chapter7

---
+++ HttpServlet.java
@@ -0,0 +1,7 @@
+package sec03.exam03;
+
+public abstract class HttpServlet {
+	
+	public abstract void service();
+	
+}
---
+++ LoginServlet.java
@@ -0,0 +1,8 @@
+package sec03.exam03;
+
+public class LoginServlet extends HttpServlet{
+
+	public void service() {
+		System.out.println("로그인 합니다.");
+	}
+}
