19c5afc63a5fedfafb3ea8532af40c52fbc52180
Hansol <hansol4412@naver.com>
2020-05-01 22:18:08 +0900

    chpater 11-2

---
+++ MemberExample.java
@@ -0,0 +1,14 @@
+package chap11;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		//toString() 메소드 이용
+		Member member = new Member("blue","이파란");
+		System.out.println(member);
+		
+		//매개값으로 객체를 주면 객체의  toString() 메소드를 호출해 리턴값을 받아 출력한다
+
+	}
+
+}
