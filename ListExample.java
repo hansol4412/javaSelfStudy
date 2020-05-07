4698889e994a8165a1343a967afb35a6bc459c80
Hansol <hansol4412@naver.com>
2020-05-07 20:18:08 +0900

    chapter13-1

---
+++ ListExample.java
@@ -0,0 +1,15 @@
+package chap13;
+import java.util.List;
+public class ListExample {
+
+	public static void main(String[] args) {
+		BoardDao boardDao = new BoardDao();
+		
+		List<Board> list = boardDao.getBoardList();
+		for(Board board : list) {
+			System.out.println(board.getTitle() + "-" + board.getContent());
+		}
+
+	}
+
+}
